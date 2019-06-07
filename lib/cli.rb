2390185077ab2d3b2e8cf82be8858f998bc6f84d
aparkening <aaron.parkening@gmail.com>
2019-06-07 16:42:39 -0400

    Refactored CLI methods to use instance variables.

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,9 +1,18 @@
 class TeaShopper::CLI
 ## This class presents data and gets input from user
 
-  attr_reader :song_base_url, :song_index_url
+  # attr_reader :song_base_url, :song_index_url
+  attr_accessor :category, :subcategory, :selected_tea
 
-  # Controller
+  # Set instance variables
+  def initialize
+    @category = nil
+    @subcategory = nil
+    @selected_tea = nil
+    self.run
+  end
+
+  ##### Controller #####
   def run
     self.welcome
 
@@ -11,132 +20,85 @@ class TeaShopper::CLI
     self.make_teas
 
     # Start process of selecting a tea category, subcategory, and tea
-    self.guide_user
+    self.find_teas
   end
 
-  # Create tea instances
+  ##### Build Tea Objects #####
+  # Create initial Tea objects
   def make_teas
     # puts "We're pulling today's tea categories from the web. This may take a few moments...\n"
     tea_array = SongScraper.scrape_teas
     Tea.create_from_collection(tea_array)
   end
 
-  # Add additional attributes to tea instances from profile pages
-### scrape teas just from category  
+  # Add additional attributes to Tea objects from scraped profile pages
   def add_scraped_attributes(tea_array)
-    self.spacer
+    self.separator
+    
+    # Include note for potentially long scrape time
     puts "We're pulling today's teas from the web. This may take a few moments...\n"
 
+    # Only add to teas we need to display. Scrape from category array, not full Tea.all array.
     tea_array.each do |tea|
       attributes = SongScraper.scrape_profile_page(tea.url)
       tea.add_tea_attributes(attributes)
     end
   end
 
-  # Guide user through category, subcategory, and tea selection
-  def guide_user
-    # Main menu: get category from user
-    # category = self.display_main_menu
-    category = "type"
-    subcategory = nil
-    selected_tea = nil
+  ##### Find Teas #####
+  # Set category, subcategory, and tea selection
+  def find_teas
 
     # Display category selections, get subcategory
-    subcategory = self.display_category(category)
+    # subcategory = self.display_category
+    self.display_category
 
-    # Show subcategory menu of teas
-    selected_tea = self.display_subcategory(subcategory, category) if subcategory
+    # Show subcategory teas, get tea selection
+    # selected_tea = self.display_subcategory(subcategory, category) if @subcategory
+    self.display_subcategory if @subcategory
 
     # Display tea profile
-    self.display_tea(selected_tea) if selected_tea
+    self.display_tea if @selected_tea
 
     self.goodbye
   end
 
+  # Display menu for tea type, set @subcategory
+  def display_category
 
-#   # Display top menu and get input
-#   def display_main_menu
-#     self.section_title("Main Menu")
-
-#     # Display instructions and get input
-#     puts "Choose a method below to explore today's teas:\n(Or type 'X' to exit)"
-#     puts "\n"
-#     puts "- Type".colorize(:light_blue)
-#     # Song too complicated to include initially
-#     # puts "- Region".colorize(:light_blue)
-#     puts "- Shop\n".colorize(:light_blue)
-#     input = gets.strip.downcase
-
-# ### Turn into method
-#     # If input is x or exit, say goodbye
-#     if input == "x" || input == "exit"
-#       self.goodbye
-#     # Else check valid input
-#     elsif (input != "type") && (input != "region") && (input != "shop")
-#       puts "Your input wasn't recognized, so we'll list tea by Type."
-#       input = "type"
-#     end
-
-#     return input
-#   end
-
-### Improve for region and shop
-  # Display menu for tea type
-  def display_category(category)
-    # Display instructions and potential selections
-    # title = "Tea " + category.capitalize
-    # self.section_title(title)
+    # Display instructions
     puts "Choose a category below find your next great tea:\n(Or type 'X' to exit)"
     puts "\n"
 
-    # Display list of tea categories
-    # case category
-    #   when "shop"
-    #     Tea.shops.each { |obj| puts "- #{obj.capitalize}".colorize(:light_blue)}
-    #     puts "\n"
-    #   when "region"
-    #     Tea.regions.each { |obj| puts "- #{obj.capitalize}".colorize(:light_blue)}
-    #     puts "\n"
-    #   else
-        Tea.types.each { |obj| puts "- #{obj.capitalize}".colorize(:light_blue)}
-        puts "\n"
-    # end
-
-    # Get input, match to next step
+    # Display today's tea types, get input
+    Tea.types.each { |obj| puts "- #{obj.capitalize}".colorize(:light_blue)}
+    puts "\n"
     input = gets.strip.downcase
     
     # Ensure valid input
     if self.exit?(input) 
       # return self.goodbye
-      input = nil
+      return @subcategory = nil
     elsif input.include?("black")
-      input = "black/red"
+      return @subcategory = "black/red"
     elsif Tea.types.none?{|obj| obj == input}
-      puts "\nHmmm, we don't recognize that type of tea, so we'll show you our favorite: Black/red teas!"
-      input = "black/red"
+      puts "\nHmmm, we don't recognize that type of tea, so we'll show you our favorite: Black/red teas..."
+      return @subcategory = "black/red"
     end
-
-    return input 
+  
+    return @subcategory = input
   end
 
-  # Display submenu with numbered list of teas, return tea object
-  def display_subcategory(subcategory, category)
-
+  # Display ordered list of teas (alphabetically sorted), set @selected_tea
+  def display_subcategory
     # Assign teas to display
-    # case category
-    #   when "shop"
-    #     teas = Tea.teas_by_shop(subcategory)
-    #   when "region"
-    #     teas = Tea.teas_by_region(subcategory)
-    #   else
-        teas = Tea.teas_by_type(subcategory)
-    # end
-
-    # Scrape profile attributes to subset of tea objects
+    teas = Tea.teas_by_type(@subcategory)
+    
+    # Scrape profile attributes for subset of tea objects
     self.add_scraped_attributes(teas)
 
     # Display title and instructions
-    title = subcategory.capitalize + " Tea"
+    title = @subcategory.capitalize + " Tea"
     self.section_title(title)    
     self.list_instructions
 
@@ -153,17 +115,16 @@ class TeaShopper::CLI
 
       # If input is a number, check for range and find tea by index
       if self.convert_to_index(input).between?(0,teas.length)
-        return teas[self.convert_to_index(input)]
-      # Else check if input is a tea name
+        return @selected_tea = teas[self.convert_to_index(input)]
+      # Else return tea if name exists
       elsif teas.find{|obj| obj.name.downcase == input}
-        return teas.find{|obj| obj.name.downcase == input}
-      # If exit, send goodbye
+        return @selected_tea = teas.find{|obj| obj.name.downcase == input}
+      # If exit, keep at nil, send goodbye
       elsif self.exit?(input) 
-        # return self.goodbye
-        return nil
+        input = nil
       else
-        self.spacer
-        puts "We don't recognize that tea, so we'll show the list again.\n\n"
+        self.separator
+        puts "We don't recognize that tea, so we'll show the list again...\n\n"
         self.list_instructions
       end
     end
@@ -187,13 +148,14 @@ class TeaShopper::CLI
   # end
 
   # Take in name of tea, find tea object, display all details
-  def display_tea(tea)
+  def display_tea
+    tea = @selected_tea
 
     # Name
     title = "#{tea.name} (#{tea.type} tea)"
     
     # self.section_title(title)
-    self.spacer 
+    self.separator 
     puts title.colorize(:green)
     puts tea.url.colorize(:light_blue)
     puts "\n"
@@ -218,7 +180,8 @@ class TeaShopper::CLI
     puts "\n"
 
     input = gets.strip.downcase
-    # case input
+    next_input = nil
+
     if input == "d"
       desc_title = tea.name + " Description:".colorize(:green)
       self.section_title(desc_title)
@@ -232,19 +195,18 @@ class TeaShopper::CLI
       puts "\n"
 
       next_input = gets.strip.downcase
-      self.guide_user if next_input == "m"
-        # else
-        #   self.goodbye
-    elsif input == "m"
-        self.guide_user
-    # else
-    #   self.goodbye
     end
+
+    # If M is selected, return to the main menu    
+    self.find_teas if input == "m" || next_input == "m"
+
+
+
   end
 
 
   ##### Display Helpers #####
-  
+
   # Welcome message
   def welcome
     self.section_title("Welcome to Tea Shopper!")
@@ -255,30 +217,29 @@ class TeaShopper::CLI
     puts "\nThanks for stopping by. Happy tea drinking!\n\n"
   end
 
-  # If input is exit, say goodbye
+  # Check if input is exit
   def exit?(input)
     input.downcase == "x" || input.downcase == "exit"
   end
 
   # Submenu instructions
   def list_instructions
-    # puts "Choose a tea number from the list below to get more details and buying info."
     puts "Choose a number or tea name from the list below get the details.\n(Or type 'X' to exit)"
     puts "\n"
   end
   
-  # Reusable display spacer
-  def spacer
+  # Section separator
+  def separator
     puts "\n----------\n\n"
   end
 
-  # Title for section, including spacer
+  # Title for section, including separator
   def section_title(title)
-    self.spacer 
+    self.separator 
     puts title.colorize(:green) + "\n\n"
   end
 
-  # Display numbered list of tea names
+  # Display numbered list from array input
   def num_list(array)
     array.each.with_index(1) { |obj, index| puts "#{index}. #{obj.name} ($#{obj.price_per_oz} per oz., #{obj.shop_name})".colorize(:light_blue) }
   end
