96f0b00ffdc1b83dfabf574db64e567c4f84cadc
aparkening <aaron.parkening@gmail.com>
2019-06-07 15:52:48 -0400

    Fixed console bug. Renamed main run file to tea_shopper to conform with Bundler gem setup.

--- config/environment.rb
+++ config/environment.rb
@@ -3,16 +3,10 @@ require 'open-uri'
 require "bundler/setup"
 Bundler.require(:default, :development)
 
-# require 'colorize'
-# require 'nokogiri'
-# require 'pry'
-
 require_relative '../lib/tea_shopper/version'
 require_relative '../lib/song_scraper'
 require_relative '../lib/tea'
 require_relative '../lib/cli'
-
-# require './config/environment/'
 
 # module TeaShopper
 #   class Error < StandardError; end
