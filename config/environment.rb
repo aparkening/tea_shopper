e69eebac352f8a7625d2310114cda55aef1b9534
aparkening <aaron.parkening@gmail.com>
2019-06-11 16:43:45 -0700

    Removed extraneous TeaShopper module call from environment.rb.

--- config/environment.rb
+++ config/environment.rb
@@ -7,8 +7,3 @@ require_relative '../lib/tea_shopper/version'
 require_relative '../lib/song_scraper'
 require_relative '../lib/tea'
 require_relative '../lib/cli'
-
-# module TeaShopper
-#   class Error < StandardError; end
-#   # Your code goes here...
-# end
