423a12f67058564618914a04ced39f399c0ad1d8
JenDirmeyer <dirmeyer@gmail.com>
2019-07-24 14:59:57 -0400

    write spec for has teas

--- spec/tea_shopper_spec.rb
+++ spec/tea_shopper_spec.rb
@@ -1,5 +1,10 @@
 require 'pry'
 RSpec.describe TeaShopper do
+  before do
+    tea_array = TeaShopper::SongScraper.new.scrape_teas
+    TeaShopper::Tea.create_from_collection(tea_array)
+  end 
+
   it "has a version number" do
     expect(TeaShopper::VERSION).not_to be nil
   end
@@ -7,5 +12,9 @@ RSpec.describe TeaShopper do
   # it "does something useful" do
   #   expect(false).to eq(true)
   # end
+
+  it "has teas" do 
+    expect(TeaShopper::Tea.all).not_to eq("")
+  end 
 
 end
